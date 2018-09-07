f66c6a0a6d26069ca1beafa6b79ab63d70f732fc
ericloeliger <jon.loeliger@gmail.com>
2018-09-07 06:26:45 -0500

    added support for multiple indoor sensors

--- environment_monitor.py
+++ environment_monitor.py
@@ -7,10 +7,9 @@
 # 12/29/17 - Changed from #!/usr/local/bin/python3.6 to #!/usr/bin/python3
 # 12/30/17 - Changed to storing all meter readings in a single row, improved exception logging
 # 12/31/17 - Added sending to adafruit IO
+# 9/5/18 - Added support for multiple local sensors
 
 # To Do
-# Add NOAA temp data
-
 
 
 import logging
@@ -53,30 +52,10 @@ if debugMode == 1:
 # functions go here
 
 # ######### Main Program ############
-
-
-
-# program goes here
 try:
     logger.info("########## Begin environment_monitor.py ##########")
     logger.info("Opening DB connection")
     sql.openDBConnection()
-    # call API to get environment vaues
-    
-    # callApi(method, url, authType, authorization, accept, body, expectJSON):
-    url = config['sensor.config']['Sensor1URL']
-    sensor_id = config['sensor.config']['Sensor1ID']
-    (response,response_code) = callApi('GET', url, None, None, None,"", 0)
-    logger.debug("Raw binary response: %s" % response.encode('utf-8'))
-    response = response.replace('\r', '')
-    list_data = response.split(",")
-    sensor_type = list_data[0]
-    temperature = list_data[1]
-    humidity = list_data[2]
-    logger.debug("Parameter 1 =%s" % sensor_type)
-    logger.debug("Parameter 2 =%s" % temperature)
-    logger.debug("Parameter 3 =%s" % humidity)
-
 
     # call NOAA API to get outside temp
     noaa_base_url = config['noaa.api.config']['BaseURL']
@@ -106,51 +85,82 @@ try:
         outdoor_temperature = None
         outdoor_timestamp = None
 
-    # save values in DB
-    # insertMeterReading - columns: id (auto), type_code, value, sensor_id, timestamp (auto)
-    # insertMeterReadings - columns: id (auto), sensor_id, indoor_temperature, indoor_humidity, outdoor_temperature, timestamp (auto)
-    insert_dict = {}
-
-    # depreciated 12/30/17
-##    if temperature != None:
-##        insert_dict['type_code'] = 1
-##        insert_dict['value'] = temperature
-##        insert_dict['sensor_id'] = sensor_id
-##        result = sql.insertMeterReading(insert_dict)
-##        logger.info("Insert temperature result: %s" % result)
-##
-##    if humidity != None:
-##        insert_dict['type_code'] = 2
-##        insert_dict['value'] = humidity
-##        insert_dict['sensor_id'] = sensor_id
-##        result = sql.insertMeterReading(insert_dict)
-##        logger.info("Insert humidity result: %s" % result)
-
-    insert_dict['sensor_id'] = sensor_id
-    insert_dict['indoor_temperature'] = temperature
-    insert_dict['indoor_humidity'] = humidity
-    insert_dict['outdoor_temperature'] = outdoor_temperature
-    insert_dict['outdoor_timestamp'] = outdoor_timestamp
-    result = sql.insertMeterReadings(insert_dict)
-    logger.info("Insert meter readings result: %s" % result)
 
 
-    # sending to adafruit IO feed
+
+    # setup to adafruit IO feed
     aio = Client(config['adafruit.io']['ClientKey'])
 
+    # initialize adafruit dict
     # define feed dictionary
     feedDictionary = {}
-    feedDictionary['indoorTemperature'] = {}
-    feedDictionary['indoorHumidity'] = {}
     feedDictionary['outdoorTemperature'] = {}
-    feedDictionary['indoorTemperature']['feedID'] = config['adafruit.io']['IndoorTemperatureFeedID1']
-    feedDictionary['indoorHumidity']['feedID'] = config['adafruit.io']['IndoorHumidityFeedID1']
     feedDictionary['outdoorTemperature']['feedID'] = config['adafruit.io']['OutdoorTemperatureFeedID1']
-
-    # add test data to feed dictionary
-    feedDictionary['indoorTemperature']['value'] = temperature
-    feedDictionary['indoorHumidity']['value'] = humidity
     feedDictionary['outdoorTemperature']['value'] = outdoor_temperature
+
+
+
+
+    # import quantity of sensors
+    sensor_qty = int(config['sensor.1']['SensorQuantity'])
+    current_sensor = 1
+
+    while current_sensor <= sensor_qty:
+        logger.info("Current sensor: %s" % current_sensor)
+        # callApi(method, url, authType, authorization, accept, body, expectJSON):
+        sensor_config_id = 'sensor.%s' % current_sensor
+        url = config[sensor_config_id]['SensorURL']
+        sensor_id = config[sensor_config_id]['SensorID']
+        sensor_version = int(config[sensor_config_id]['SensorVersion'])
+        (response,response_code) = callApi('GET', url, None, None, None,"", 0)
+        logger.debug("Raw binary response: %s" % response.encode('utf-8'))
+        response = response.replace('\r', '')
+        list_data = response.split(",")
+        if sensor_version == 1:
+            sensor_type = list_data[0]
+            temperature = list_data[1]
+            humidity = list_data[2]
+        elif sensor_version == 2:
+            sensor_type = list_data[1]
+            temperature = list_data[2]
+            humidity = list_data[3]
+        else:
+            logger.error("Unsupported sensor version")
+        logger.debug("Parameter 1 =%s" % sensor_type)
+        logger.debug("Parameter 2 =%s" % temperature)
+        logger.debug("Parameter 3 =%s" % humidity)
+
+        # save values in DB
+        # insertMeterReading - columns: id (auto), type_code, value, sensor_id, timestamp (auto)
+        # insertMeterReadings - columns: id (auto), sensor_id, indoor_temperature, indoor_humidity, outdoor_temperature, timestamp (auto)
+        insert_dict = {}
+        insert_dict['sensor_id'] = sensor_id
+        insert_dict['indoor_temperature'] = temperature
+        insert_dict['indoor_humidity'] = humidity
+        insert_dict['outdoor_temperature'] = outdoor_temperature
+        insert_dict['outdoor_timestamp'] = outdoor_timestamp
+        result = sql.insertMeterReadings(insert_dict)
+        logger.info("Insert meter readings result: %s" % result)
+
+        # build adafruit dict
+        temp_key = 'indoorTemperature%s' % current_sensor
+        humidity_key = 'indoorHumidity%s' % current_sensor
+        temp_feed_prop_id = 'IndoorTemperatureFeedID%s' % current_sensor
+        humidity_feed_prop_id = 'IndoorHumidityFeedID%s' % current_sensor
+
+
+        feedDictionary[temp_key] = {}
+        feedDictionary[humidity_key] = {}
+        feedDictionary[temp_key]['feedID'] = config['adafruit.io'][temp_feed_prop_id]
+        feedDictionary[humidity_key]['feedID'] = config['adafruit.io'][humidity_feed_prop_id]
+
+        # add test data to feed dictionary
+        feedDictionary[temp_key]['value'] = temperature
+        feedDictionary[humidity_key]['value'] = humidity
+
+        # increment sensor counter
+        current_sensor = current_sensor + 1
+
 
     logger.debug("Adafruit feed dictionary: %s" % feedDictionary)
 
